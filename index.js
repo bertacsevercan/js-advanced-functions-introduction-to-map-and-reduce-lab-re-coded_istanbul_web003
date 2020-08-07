73ff8ac4eace7b56d42097c01115134bf8ebdadf
bertac_severcan <bertac.severcan@icloud.com>
2020-08-07 23:28:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    const newArr = []
+    for(const el of sourceArray){
+        newArr.push(el * -1)
+    }
+    return newArr;
+}
+function mapToNoChange(sourceArray){
+    return sourceArray
+    
+}
+function mapToDouble(sourceArray){
+    const newArr = []
+    for(const el of sourceArray){
+        newArr.push(el * 2)
+    }
+    return newArr;
+}
+function mapToSquare(sourceArray){
+    const newArr = []
+    for(const el of sourceArray){
+        newArr.push(el ** 2)
+    }
+    return newArr;
+    
+}
+function reduceToTotal(sourceArray, startingPoint=0){
+    let total = startingPoint;
+    for(let el of sourceArray){
+        total += el;
+    }
+    return total
+}
+function reduceToAllTrue(sourceArray){
+    let sentinal = false;
+    for(const el of sourceArray){
+        if(el){
+            sentinal = true;
+        }
+        else{
+            return false
+        }   
+    }
+    return sentinal
+    
+}
+function reduceToAnyTrue(sourceArray){
+    let sentinal = true;
+    for(const el of sourceArray){
+        if(!el){
+            sentinal = false;
+        }
+        else{
+            return true
+        }   
+    }
+    return sentinal
+    
+}
